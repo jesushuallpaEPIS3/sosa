acdb9a2d78fe859c102d8a8fc7a4cfad66e27f07
Jesus Antonio Huallpa Maron <102675596+jesushuallpaEPIS3@users.noreply.github.com>
2024-07-12 11:46:49 -0500

    ver57

--- src/MODEL/Usuario.php
+++ src/MODEL/Usuario.php
@@ -1,26 +1,28 @@
 <?php
 namespace App\Model;
 
-use DB\Conectar;
+use mysqli;
 
-class Calculadora {
-    public function sumar($a, $b) {
-        return $a + $b;
-    }
+class Usuario {
+    public static function conexion() {
+        // Cargar las propiedades desde el archivo de configuración
+        $config = parse_ini_file(__DIR__ . '/../../config.ini', true);
 
-    public function restar($a, $b) {
-        return $a - b;
-    }
+        // Obtener las credenciales del archivo de configuración
+        $host = $config['database']['url'];
+        $username = $config['database']['username'];
+        $password = $config['database']['password'];
+        $dbname = $config['database']['dbname'];
 
-    public function multiplicar($a, $b) {
-        return $a * $b;
-    }
+        // Establecer la conexión con la base de datos
+        $conexion = new mysqli($host, $username, $password, $dbname);
 
-    public function dividir($a, $b) {
-        if ($b == 0) {
-            throw new \InvalidArgumentException("División por cero");
-        }
-        return $a / $b;
+        // Verificar si la conexión fue exitosa
+
+        // Establecer el conjunto de caracteres a utf8
+        $conexion->set_charset('utf8');
+
+        return $conexion;
     }
 }
 ?>
--- tests/MODEL/UsuarioTest.php
+++ tests/MODEL/UsuarioTest.php
@@ -1,34 +1,13 @@
 <?php
+namespace Tests\MODEL;
+
 use PHPUnit\Framework\TestCase;
-use App\Model\Usuario;
+use Model\Usuario;
 
-class CalculadoraTest extends TestCase {
-    public function testSumar() {
-        $calculadora = new Calculadora();
-        $this->assertEquals(4, $calculadora->sumar(2, 2));
-    }
-
-    public function testRestar() {
-        $calculadora = new Calculadora();
-        $this->assertEquals(0, $calculadora->restar(2, 2));
-    }
-
-    public function testMultiplicar() {
-        $calculadora = new Calculadora();
-        $this->assertEquals(4, $calculadora->multiplicar(2, 2));
-    }
-
-    public function testDividir() {
-        $calculadora = new Calculadora();
-        $this->assertEquals(2, $calculadora->dividir(4, 2));
-    }
-
-    public function testDividirPorCero() {
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("División por cero");
-
-        $calculadora = new Calculadora();
-        $calculadora->dividir(4, 0);
+class UsuarioTest extends TestCase {
+    public function testConexion() {
+        $conexion = Conectar::conexion();
+        $this->assertInstanceOf(\mysqli::class, $conexion);
     }
 }
 ?>
