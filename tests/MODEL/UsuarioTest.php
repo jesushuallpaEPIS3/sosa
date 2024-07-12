22b150507f67ae3a44f51d3f2907b443cb0748c8
Jesus Antonio Huallpa Maron <102675596+jesushuallpaEPIS3@users.noreply.github.com>
2024-07-12 11:31:12 -0500

    ver54

---
+++ tests/MODEL/UsuarioTest.php
@@ -0,0 +1,100 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Model\Usuario;
+
+class UsuarioTest extends TestCase {
+    private $usuario;
+    private $dbMock;
+
+    protected function setUp(): void {
+        // Mock the database connection
+        $this->dbMock = $this->createMock(mysqli::class);
+
+        // Mock the Conectar class
+        $conectarMock = $this->getMockBuilder('DB\Conectar')
+                             ->setMethods(['conexion'])
+                             ->getMock();
+        $conectarMock->expects($this->any())
+                     ->method('conexion')
+                     ->willReturn($this->dbMock);
+
+        // Inject the mock into the Usuario class
+        $this->usuario = new Usuario();
+        $this->usuario->db = $this->dbMock;
+    }
+
+    public function testLoginSuccess() {
+        // Arrange
+        $username = 'jesus';
+        $password = '123';
+
+        $stmtMock = $this->createMock(mysqli_stmt::class);
+        $resultMock = $this->createMock(mysqli_result::class);
+
+        // Configure the result mock
+        $resultMock->expects($this->once())
+                   ->method('fetch_assoc')
+                   ->willReturn(['id' => 1, 'usuario' => $username]);
+
+        // Configure the statement mock
+        $stmtMock->expects($this->once())
+                 ->method('bind_param')
+                 ->with('ss', $username, $password);
+        $stmtMock->expects($this->once())
+                 ->method('execute')
+                 ->willReturn(true);
+        $stmtMock->expects($this->once())
+                 ->method('get_result')
+                 ->willReturn($resultMock);
+
+        // Configure the database mock
+        $this->dbMock->expects($this->once())
+                     ->method('prepare')
+                     ->with('SELECT * FROM tbadmin WHERE usuario = ? AND password = ?')
+                     ->willReturn($stmtMock);
+
+        // Act
+        $result = $this->usuario->login($username, $password);
+
+        // Assert
+        $this->assertEquals(['id' => 1, 'usuario' => $username], $result);
+    }
+
+    public function testLoginFailure() {
+        // Arrange
+        $username = 'wronguser';
+        $password = 'wrongpass';
+
+        $stmtMock = $this->createMock(mysqli_stmt::class);
+        $resultMock = $this->createMock(mysqli_result::class);
+
+        // Configure the result mock
+        $resultMock->expects($this->once())
+                   ->method('fetch_assoc')
+                   ->willReturn(null);
+
+        // Configure the statement mock
+        $stmtMock->expects($this->once())
+                 ->method('bind_param')
+                 ->with('ss', $username, $password);
+        $stmtMock->expects($this->once())
+                 ->method('execute')
+                 ->willReturn(true);
+        $stmtMock->expects($this->once())
+                 ->method('get_result')
+                 ->willReturn($resultMock);
+
+        // Configure the database mock
+        $this->dbMock->expects($this->once())
+                     ->method('prepare')
+                     ->with('SELECT * FROM tbadmin WHERE usuario = ? AND password = ?')
+                     ->willReturn($stmtMock);
+
+        // Act
+        $result = $this->usuario->login($username, $password);
+
+        // Assert
+        $this->assertNull($result);
+    }
+}
+?>
